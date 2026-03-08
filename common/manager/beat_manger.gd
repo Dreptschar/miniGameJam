45ed5f0d5c0a5471d658ae2cb524d2b384d5db05
Jonas Escher <jonas@backpapier.li>
2026-03-08 15:29:22 +0100

    feat: use audio busses for leveling

--- common/manager/beat_manger.gd
+++ common/manager/beat_manger.gd
@@ -12,40 +12,57 @@ signal beat_hit(index: int)
 @export var use_music_clock: bool = true
 @export var music_autoplay: bool = true
 @export_range(0.0, 8.0, 0.001) var music_start_offset_sec: float = 0.0
-@export_range(200.0, 22000.0, 10.0) var normal_lowpass_cutoff_hz: float = 18000.0
-@export_range(200.0, 22000.0, 10.0) var frozen_lowpass_cutoff_hz: float = 1800.0
-@export_range(0.1, 4.0, 0.1) var normal_lowpass_resonance: float = 0.8
-@export_range(0.1, 4.0, 0.1) var frozen_lowpass_resonance: float = 1.4
-@export_range(0.5, 2.0, 0.01) var normal_pitch_scale: float = 1.0
-@export_range(0.5, 2.0, 0.01) var frozen_pitch_scale: float = 0.96
+
+@export_group("Audio Buses")
+@export var beat_bus: StringName = &"Beat"
+@export var freeze_bus: StringName = &"Freeze"
+@export var music_bus: StringName = &"Music"
 
 var _beat_time: float = 0.0
 var _beat_index: int = 0
 var _audio_player: AudioStreamPlayer
+var _freeze_player: AudioStreamPlayer
 var _music_player: AudioStreamPlayer
 var _active_frozen_objects: int = 0
-var _beat_bus_name: StringName = &"BeatMangerBus"
-var _beat_bus_index: int = -1
-var _lowpass_effect: AudioEffectLowPassFilter
 var _last_song_time_sec: float = 0.0
 var _pending_music_stream: AudioStream = null
 
+
 func _ready() -> void:
+	_ensure_bus(beat_bus)
+	_ensure_bus(freeze_bus)
+	_ensure_bus(music_bus)
+
 	_audio_player = AudioStreamPlayer.new()
 	_audio_player.stream = beat_sound
 	_audio_player.volume_db = beat_volume_db
-	_ensure_audio_bus()
-	if _beat_bus_index >= 0:
-		_audio_player.bus = _beat_bus_name
+	_audio_player.bus = beat_bus
 	add_child(_audio_player)
+
+	_freeze_player = AudioStreamPlayer.new()
+	_freeze_player.stream = freeze_sound
+	_freeze_player.volume_db = beat_volume_db
+	_freeze_player.bus = freeze_bus
+	add_child(_freeze_player)
+
 	_music_player = AudioStreamPlayer.new()
 	_music_player.stream = music_stream
 	_ensure_music_stream_loop(_music_player.stream)
 	_music_player.volume_db = music_volume_db
+	_music_player.bus = music_bus
 	_music_player.finished.connect(_on_music_finished)
 	add_child(_music_player)
-	_music_player.pitch_scale = 1.0
 	_last_song_time_sec = 0.0
+
+
+func _ensure_bus(bus_name: StringName) -> void:
+	if AudioServer.get_bus_index(bus_name) >= 0:
+		return
+	var idx := AudioServer.bus_count
+	AudioServer.add_bus(idx)
+	AudioServer.set_bus_name(idx, bus_name)
+	AudioServer.set_bus_send(idx, &"Master")
+
 
 func _process(delta: float) -> void:
 	if _is_music_clock_active():
@@ -87,24 +104,23 @@ func _process_music_clock() -> void:
 
 	_last_song_time_sec = song_time
 
+
 func _play_beat_sound() -> void:
-	if _audio_player == null:
-		return
 	if _pending_music_stream != null:
 		music_stream = _pending_music_stream
 		_pending_music_stream = null
 		_music_player.stop()
 		_music_player.stream = music_stream
 		_ensure_music_stream_loop(_music_player.stream)
-		_music_player.pitch_scale = 1.0
 		_last_song_time_sec = 0.0
-	var sound := freeze_sound if _active_frozen_objects > 0 and freeze_sound != null else beat_sound
-	if sound == null:
-		return
-	_audio_player.stream = sound
-	_audio_player.volume_db = beat_volume_db
-	_audio_player.pitch_scale = normal_pitch_scale
-	_audio_player.play()
+
+	if _active_frozen_objects > 0:
+		if freeze_sound != null and _freeze_player != null:
+			_freeze_player.play()
+	else:
+		if beat_sound != null and _audio_player != null:
+			_audio_player.play()
+
 	if not _music_player.playing and music_stream != null and music_autoplay:
 		_music_player.play()
 		_last_song_time_sec = 0.0
@@ -115,13 +131,15 @@ func notify_object_frozen_state_changed(is_frozen: bool) -> void:
 		_active_frozen_objects += 1
 	else:
 		_active_frozen_objects = max(_active_frozen_objects - 1, 0)
-	_update_frozen_audio_state()
+
 
 func get_seconds_per_beat() -> float:
 	return 60.0 / bpm
 
+
 func get_time_since_last_beat() -> float:
 	return _beat_time
+
 
 func get_time_until_next_beat() -> float:
 	return get_seconds_per_beat() - _beat_time
@@ -142,7 +160,6 @@ func set_music_stream(stream: AudioStream, restart: bool = true, start_position_
 func _ensure_music_stream_loop(stream: AudioStream) -> void:
 	if stream == null:
 		return
-
 	for property_info in stream.get_property_list():
 		if property_info.get("name", "") == "loop":
 			stream.set("loop", true)
@@ -150,45 +167,14 @@ func _ensure_music_stream_loop(stream: AudioStream) -> void:
 
 
 func _on_music_finished() -> void:
-	if _music_player == null:
+	if _music_player == null or music_stream == null or not music_autoplay:
 		return
-	if music_stream == null:
-		return
-	if not music_autoplay:
-		return
-
 	_music_player.play(0.0)
 	_last_song_time_sec = 0.0
 
 
-func _ensure_audio_bus() -> void:
-	var existing_index: int = AudioServer.get_bus_index(_beat_bus_name)
-	if existing_index >= 0:
-		_beat_bus_index = existing_index
-	else:
-		_beat_bus_index = AudioServer.bus_count
-		AudioServer.add_bus(_beat_bus_index)
-		AudioServer.set_bus_name(_beat_bus_index, _beat_bus_name)
-		AudioServer.set_bus_send(_beat_bus_index, &"Master")
-
-	_lowpass_effect = AudioServer.get_bus_effect(_beat_bus_index, 0) as AudioEffectLowPassFilter
-	if _lowpass_effect == null:
-		_lowpass_effect = AudioEffectLowPassFilter.new()
-		AudioServer.add_bus_effect(_beat_bus_index, _lowpass_effect, 0)
-	_update_frozen_audio_state()
-
-
-func _update_frozen_audio_state() -> void:
-	if _lowpass_effect == null:
-		return
-
-	var is_frozen_audio: bool = _active_frozen_objects > 0
-	_lowpass_effect.cutoff_hz = frozen_lowpass_cutoff_hz if is_frozen_audio else normal_lowpass_cutoff_hz
-	_lowpass_effect.resonance = frozen_lowpass_resonance if is_frozen_audio else normal_lowpass_resonance
-
-
 func _is_music_clock_active() -> bool:
-	return use_music_clock and music_stream != null and _music_player != null
+	return use_music_clock and music_stream != null and _music_player != null and _music_player.playing
 
 
 func _get_song_time_sec() -> float:
